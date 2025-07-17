e35cab79e6c747f7fabd8d437b630604c14c25ff
Denis Ternovoy <denis.ternovoi@mail.ru>
2025-07-17 22:30:53 +0300

    Fix mistakes

--- src/utils.py
+++ src/utils.py
@@ -7,9 +7,6 @@ from src.product import Product
 def read_json_data(path: str) -> list[Category]:
     """Функция осуществляет подгрузку данных по категориям и товарам из файла JSON"""
 
-    Category.category_count = 0  # без этого в тестах сохраняется состояние класса, что приводит к AssertionError
-    Category.product_count = 0  # без этого в тестах сохраняется состояние класса, что приводит к AssertionError
-
     list_of_categories = []
 
     with open(path, "r", encoding="utf-8") as file:
