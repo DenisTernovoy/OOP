948f0a022a48c1c0fcc81c8fb75289338a9d238c
Denis Ternovoy <denis.ternovoi@mail.ru>
2025-07-26 15:01:48 +0300

    Task_2_completed

--- main.py
+++ main.py
@@ -1,36 +1,7 @@
 from src.product import Product
 from src.category import Category
-<<<<<<< HEAD
 from src.iterator import CategoryIterator
 
-
-if __name__ == '__main__':
-    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
-    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-
-    print(str(product1))
-    print(str(product2))
-    print(str(product3))
-
-    category1 = Category(
-        "Смартфоны",
-        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-        [product1, product2, product3]
-    )
-
-    print(str(category1))
-
-    print(category1.products)
-
-    print(product1 + product2)
-    print(product1 + product3)
-    print(product2 + product3)
-
-    cat_iter = CategoryIterator(category1)
-    for i in cat_iter:
-        print(i)
-=======
 from src.smartphone import Smartphone
 from src.lawngrass import LawnGrass
 
@@ -115,4 +86,3 @@ if __name__ == '__main__':
         print("Возникла ошибка TypeError при добавлении не продукта")
     else:
         print("Не возникла ошибка TypeError при добавлении не продукта")
->>>>>>> e8eb5d0 (Task_1_completed)
